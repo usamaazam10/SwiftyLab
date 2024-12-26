52024a4b6d326ebe84a6af38cb1814d475bed90f
Usama Azam <usamaazam10@gmail.com>
2024-12-26 10:28:15 +0500

    Initial Commit

---
+++ SwiftyLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftyLab
+//
+//  Created by Usama Azam on 26/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftyLab/SwiftyLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLabApp.swift
+//  SwiftyLab
+//
+//  Created by Usama Azam on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
